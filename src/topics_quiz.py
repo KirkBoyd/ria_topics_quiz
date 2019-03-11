014fbb30f6c59c47834930097d1183c5e079d46b
Kirk Boyd <kirkland.boyd@trincoll.edu>
2019-03-11 04:21:59 +0000

    add logic so robot does not hit the wall.

--- src/topics_quiz.py
+++ src/topics_quiz.py
@@ -4,7 +4,7 @@ import rospy
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import LaserScan
 vel = Twist()
-vel.linear.x = 0
+vel.linear.x = 0.5
 vel.linear.y = 0
 vel.linear.z = 0
 vel.angular.x = 0
@@ -14,9 +14,19 @@ turn = False
 
 
 def callback(msg):
+    print (msg.ranges[180])
+    if (msg.ranges[360] < 1.5):
+        vel.linear.x = 0
+        vel.angular.z = 0.25
+    elif (msg.ranges[180] < 1):
+        vel.linear.x = 0
+        vel.angular.z = 0.25
+    else:
+        vel.linear.x = 0.5
+        vel.angular.z = 0
 
 
-rospy.init_node('infinity_node')
+rospy.init_node('topics_quiz_node')
 pub = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
 sub = rospy.Subscriber('/kobuki/laser/scan', LaserScan, callback)
 rate = rospy.Rate(2)
